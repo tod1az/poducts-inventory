b6fbbd896c0136013cd2189d40e521745b274caf
tod1az <t.diaz.soto@gmail.com>
2024-06-19 18:20:10 -0400

    fix article key is now unique

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,12 +1,15 @@
 import Form from "./Form"
 import { getProducts } from "@/server/queries"
 
-export default async function ProductsList() {
-  const products = await getProducts()
+export default async function ProductsList({ query }: { query: string }) {
+  const products = await getProducts(query)
+
+  if (products?.length === 0 || !products) return <h1>Sin resultados</h1>
+
   return (
     <section className="grid gap-2">
       {products.map((product) => (
-        <article className="bg-gray-700 p-4" key={product.code}>
+        <article className="bg-gray-700 p-4" key={product.id}>
           <p>{product.code}</p>
           <p>{product.description}</p>
           <p>{product.family}</p>
