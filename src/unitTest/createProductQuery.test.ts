699b8c29a671fbbacb37bd34c6db0ebf2b55c8de
tod1az <t.diaz.soto@gmail.com>
2024-06-19 18:18:28 -0400

    add createProductQuery test

---
+++ src/unitTest/createProductQuery.test.ts
@@ -0,0 +1,40 @@
+import { createProductsQuery } from "../server/queryBuilder"
+import { expect, it, describe } from "vitest"
+
+describe("createProductsQuery", () => {
+  it("when there are no filters provided, should return a query without filters", () => {
+    expect(createProductsQuery({ perPage: 4 })).toStrictEqual({
+      query: "SELECT * FROM PRODUCTS LIMIT $1",
+      values: [4],
+    })
+  })
+  it("when ther are filters provided, should return a query with filters", () => {
+    expect(createProductsQuery({ perPage: 4, family: "je" })).toStrictEqual({
+      query: "SELECT * FROM PRODUCTS WHERE CODE = $1 LIMIT $2",
+      values: ["je", 4],
+    })
+    expect(
+      createProductsQuery({
+        perPage: 4,
+        family: "je",
+        description: "description",
+      })
+    ).toStrictEqual({
+      query:
+        "SELECT * FROM PRODUCTS WHERE DESCRIPTION ILIKE $1 AND CODE = $2 LIMIT $3",
+      values: ["%description%", "je", 4],
+    })
+    expect(
+      createProductsQuery({
+        perPage: 4,
+        family: "je",
+        description: "description",
+        page: 2,
+      })
+    ).toStrictEqual({
+      query:
+        "SELECT * FROM PRODUCTS WHERE DESCRIPTION ILIKE $1 AND CODE = $2 LIMIT $3 OFFSET $4",
+      values: ["%description%", "je", 4, 8],
+    })
+  })
+})
