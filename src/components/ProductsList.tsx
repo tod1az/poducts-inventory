26d6a045019b651bce09e3d2cbd8041b25cc6497
tod1az <t.diaz.soto@gmail.com>
2024-06-19 19:16:37 -0400

    implement new getProducts functionality

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,14 +1,21 @@
 import Form from "./Form"
 import { getProducts } from "@/server/queries"
 
-export default async function ProductsList({ query }: { query: string }) {
-  const products = await getProducts(query)
+export default async function ProductsList({
+  query,
+  page,
+}: {
+  query: string
+  page: string
+}) {
+  const response = await getProducts({ description: query, page })
 
-  if (products?.length === 0 || !products) return <h1>Sin resultados</h1>
+  if (response?.rows?.length === 0 || !response?.rows)
+    return <h1>Sin resultados</h1>
 
   return (
     <section className="grid gap-2">
-      {products.map((product) => (
+      {response.rows.map((product) => (
         <article className="bg-gray-700 p-4" key={product.id}>
           <p>{product.code}</p>
           <p>{product.description}</p>
