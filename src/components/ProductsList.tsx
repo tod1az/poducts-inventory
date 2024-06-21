e70800ad59e4d9e8b1d18cfb91711bd63db0161d
tod1az <t.diaz.soto@gmail.com>
2024-06-21 19:06:33 -0400

    add card component

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,26 +1,26 @@
+import { Fragment } from "react"
 import Form from "./Form"
 import { getProducts } from "@/server/queries"
+import Card from "./Card"
 
 export default async function ProductsList({
   query,
   page,
+  family,
 }: {
   query: string
   page: string
+  family: string
 }) {
-  const response = await getProducts({ description: query, page })
+  const response = await getProducts({ description: query, page, family })
   if (response?.rows?.length === 0 || !response?.rows)
     return <h1>Sin resultados</h1>
   return (
-    <section className="grid gap-2">
+    <section className="grid gap-2 items-center justify-center w-full">
       {response.rows.map((product) => (
-        <article className="bg-gray-700 p-4" key={product.id}>
-          <p>{product.code}</p>
-          <p>{product.description}</p>
-          <p>{product.family}</p>
-          <p>{product.quantity}</p>
-          <Form product={product} />
-        </article>
+        <Fragment key={product.id}>
+          <Card product={product} />
+        </Fragment>
       ))}
     </section>
   )
